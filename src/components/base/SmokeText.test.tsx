94d4a18cfdf4474def598dbc641d75ca9d5ddeb9
Сергей <tyapkin2002@mail.ru>
2022-06-14 02:04:42 +0300

    Дописал тесты

--- src/components/base/SmokeText.test.tsx
+++ src/components/base/SmokeText.test.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import SmokeText from './SmokeText'
-import { it, expect, create } from "react-test-renderer";
+import { expect, create, describe, test } from "react-test-renderer";  // я не смог установить этот пакет,
+// ведь сейчас стоит старый react, версию которого я решил не трогать (16.8.23),
+// а react-test-renderer требует react одной из версий ^16.0.0 || ^17.0.0 || ^18.0.0.
 
-it('SmokeText correctly rendering', () => {
-    const text = create(<SmokeText children={{__html: "some text"}} />).toJSON();
-    expect(text).toMatchSnapshot();
+const response = require(`../../../.storybook/responses/teaser-2.json`);
+const items = response.result.body.direct.ads;
+
+describe('Test SmokeText', function () {
+    test.each(items)('SmokeText correctly rendering', ({warning, domain, region}) => {
+        [warning, domain, region].forEach(text => {
+            const smoked = create(<SmokeText children={text}/>).toJSON();
+            expect(smoked).toMatchSnapshot();
+        });
+    });
 });
-
