ccb567944d8da1025583161a9c93e163c629bdd7
Сергей <tyapkin2002@mail.ru>
2022-06-14 18:09:28 +0300

    Добавил зависимости для тестов, но так и не понял, как подключить jest-аддон к storybook

--- src/components/base/SmokeText.test.jsx
+++ src/components/base/SmokeText.test.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import SmokeText from './SmokeText'
-import { expect, create, describe, test } from "react-test-renderer";
+import renderer from "react-test-renderer";
 
 const response = require(`../../../.storybook/responses/teaser-2.json`);
 const items = response.result.body.direct.ads;
@@ -8,7 +8,7 @@ const items = response.result.body.direct.ads;
 describe('Test SmokeText', function () {
     test.each(items)('SmokeText correctly rendering', ({warning, domain, region}, index) => {
         [warning, domain, region].forEach(text => {
-            const smoked = create(<SmokeText uid={index} children={text}/>).toJSON();
+            const smoked = renderer.create(<SmokeText uid={index} children={text}/>).toJSON();
             expect(smoked).toMatchSnapshot();
         });
     });
--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -3,10 +3,16 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 
 import RenderComponent from '../.storybook/components/RenderComponent';
+import {withTests} from "@storybook/addon-jest";
+
+
+const smokeTextResults = require('../SmokeText.jest-test-results.json');
 
 const stories = storiesOf('components', module);
-
 stories
+	.addDecorator(withTests({
+		results: smokeTextResults
+	}))
 	.add('left', () => (
 		<RenderComponent/>
 	));
